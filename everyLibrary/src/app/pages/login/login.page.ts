c4125c9cd39406caf14732bb62dedf1fb997968f
Michele Intrevado <michele.intrevado@student.univaq.it>
2021-07-11 12:48:28 +0200

    pagine login

--- everyLibrary/src/app/pages/login/login.page.ts
+++ everyLibrary/src/app/pages/login/login.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
 import { Router } from '@angular/router';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -9,9 +10,33 @@ import { Router } from '@angular/router';
 })
 export class LoginPage implements OnInit {
 
-  constructor(private navController: NavController, private router: Router) { }
+  validationUserMessage ={
+    username:[
+      {type:"required", message:"Inserisci il tuo username"},
+      {type:"pattern", message:"L'username inserito non è corretto"}
+    ],
+    password:[
+      {type:"required", message:"Inserisci la tua password"},
+      {type:"minlength", message:"La password inserita non è corretta"}
+    ]
+  }
+
+  validationFormUser: FormGroup;
+
+  constructor(private navController: NavController, private router: Router, public formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.validationFormUser=this.formBuilder.group({
+      username: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.pattern('^[a-zA-Z0-9]+$')
+      ])),
+      password: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.minLength(4)
+      ]))
+    });
+
   }
 
   signup(){
