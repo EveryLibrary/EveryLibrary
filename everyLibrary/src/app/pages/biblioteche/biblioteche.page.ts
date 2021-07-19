fdfbd8f43225d25dff130aeb6d995fd84320353c
Martina99188 <martina.nolletti@student.univaq.it>
2021-07-19 16:58:51 +0200

    google maps

--- everyLibrary/src/app/pages/biblioteche/biblioteche.page.ts
+++ everyLibrary/src/app/pages/biblioteche/biblioteche.page.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {NavController} from '@ionic/angular';
 import {Router} from '@angular/router';
+import { ViewChild, ElementRef} from '@angular/core';
+
+declare let google: any;
 
 @Component({
   selector: 'app-biblioteche',
@@ -9,6 +12,8 @@ import {Router} from '@angular/router';
 })
 export class BibliotechePage implements OnInit {
 
+  map: any;
+  @ViewChild('map', {read: ElementRef, static: false}) mapRef: ElementRef;
   constructor(private navController: NavController, private router: Router) { }
 
   ngOnInit() {
@@ -27,4 +32,16 @@ export class BibliotechePage implements OnInit {
     this.router.navigate(['/signup']);
   }
    */
+  ionViewDidEnter(){
+    this.showMap();
+  }
+  showMap(){
+    const location = new google.maps.LatLng(-17.824858,31.053028);
+    const options = {
+      center: location,
+      zoom: 15,
+      disableDefaultUI: true
+    };
+    this.map = new google.maps.Map(this.mapRef.nativeElement, options);
+  }
 }
